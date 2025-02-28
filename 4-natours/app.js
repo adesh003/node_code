c1713f3102b0a029a34ffeb66110bb5189e49f5c
Adesh <theadeshkumar03@gmail.com>
2025-02-28 22:13:11 +0530

    change

--- 4-natours/app.js
+++ 4-natours/app.js
@@ -32,12 +32,24 @@ app.get("/api/v1/tours", (req, res) => {
 
 app.get("/api/v1/tours/:id", (req, res) => {
  console.log(req.params);
+ 
+ const id = req.params.id *1;
+ 
+ if(id >tours.length)
+ {
+  return res.status(404).json({
+    status:'fail',
+    message:"Invalid id"
+  })
+ }
+ 
+ const tour = tours.find(el => el.id == id)
   res.status(200).json({
     status: "success",
-    // result: tours.length,
-    // data: {
-    //   tours,
-    // },
+    
+    data: {
+      tour
+    },
   });
 });
 
