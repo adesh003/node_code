d0cf979a3f9d77e0d70e6a65467384719c7564ac
Adesh <theadeshkumar03@gmail.com>
2025-03-09 13:56:37 +0530

    database

--- 4-natours/server.js
+++ 4-natours/server.js
@@ -1,28 +1,50 @@
-const dotenv = require('dotenv')
-const mongoose = require('mongoose')
-dotenv.config({path:'./config.env'})
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+dotenv.config({ path: './config.env' });
 
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
 
- const DB = process.env.DATABASE.replace(
-   '<PASSWORD>',
-   process.env.DATABASE_PASSWORD
- );
- 
- mongoose
-   .connect(DB, {
-     useNewUrlParser: true,
-     useUnifiedTopology: true,
-     tlsAllowInvalidCertificates: true, // Bypass SSL issues
-   })
-   .then(() => console.log('MongoDB Connected successful'))
-   
-   const tourSchemas = new mongoose.Schema({
-    name:String,
-    rating:Number,
-    price: Number
-   })
-   
-   
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    tlsAllowInvalidCertificates: true, // Bypass SSL issues
+  })
+  .then(() => console.log('MongoDB Connected successful'));
+
+const tourSchemas = new mongoose.Schema({
+  name: {
+    type: String,
+    require: [true, 'Name required'],
+    unique: true,
+  },
+  rating: {
+    type: Number,
+    default: 4.5,
+  },
+  price: {
+    type: Number,
+    require: [true, 'Must have price'],
+  },
+});
+
+const Tour = mongoose.model('Tour', tourSchemas);
+
+const testTour = new Tour({
+  name: 'the Forest Hiker',
+  rating: 4.7,
+  price: 345,
+});
+
+testTour.save().then(doc =>{
+  console.log(doc)
+}).catch(err =>{
+  console.log('ERROR😢' ,err)
+})
+
 const app = require('./app');
 console.log(process.env);
 const port = 3000;
