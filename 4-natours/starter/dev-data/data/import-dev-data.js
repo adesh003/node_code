f7c0a2c01df234ac2b2b2ca81526d04b2b7b85d5
Adesh <theadeshkumar03@gmail.com>
2025-07-01 13:01:07 +0530

    importing reviews and users data

--- 4-natours/starter/dev-data/data/import-dev-data.js
+++ 4-natours/starter/dev-data/data/import-dev-data.js
@@ -4,6 +4,10 @@ import { fileURLToPath } from 'url';
 import dotenv from 'dotenv';
 import mongoose from 'mongoose';
 import Tour from '../../../models/tourModel.js';
+import review from '../../../models/reviewModel.js';
+import User from '../../../models/userModel.js'
+import Review from '../../../models/reviewModel.js';
+
 
 // Define __dirname manually for ES modules
 const __filename = fileURLToPath(import.meta.url);
@@ -31,11 +35,19 @@ mongoose
 const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/tours.json`, 'utf-8')
 );
+const users = JSON.parse(fs.readFileSync(`${__dirname}/users.json`, 'utf-8'));
+const reviews = JSON.parse(fs.readFileSync(`${__dirname}/reviews.json`, 'utf-8'));
+
+
 
 // Import data into DB
 const importData = async () => {
   try {
-    await Tour.create(tours);
+    
+    await Tour.create(tours)
+    await User.create(users,{ validateBeforeSave:false})
+
+    await Review.create(reviews)
     console.log('Data successfully loaded');
   } catch (err) {
     console.error('Error importing data:', err);
@@ -46,7 +58,10 @@ const importData = async () => {
 // Delete all data from the collection
 const deleteData = async () => {
   try {
-    await Tour.deleteMany(); // This deletes all documents, no need to pass an array
+    await Tour.deleteMany();
+    await User.deleteMany();
+    await Review.deleteMany();
+    // This deletes all documents, no need to pass an array
     console.log('Data successfully deleted');
   } catch (err) {
     console.error('Error deleting data:', err);
